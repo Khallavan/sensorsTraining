de127e5fb301e55128dd6c72a447a1396eea2c05
Sebastian Morales <sebastianmorales.js@gmail.com>
2023-04-17 21:47:46 -0500

    implemented a DF loader
    load a dataset from csv file

--- R/load_datasets.R
+++ R/load_datasets.R
@@ -1,10 +1,19 @@
-load_datasets <- function(name) {
-    # @TODO: Implement a function to load the datasets for training the sensors model
-    # predictor
+#' @name load_datasets
+#' @title Datasets Loader
+#' @param name_dataset: character type data set name.
+#' charges a data set saved in the ./datasets folder
+#' @return sensors dataset as dataFrame with string as factor
+#' @example
+#' load_datasts(/your_dataset.csv)
+#'
+
+load_datasets <- function(name_dataset) {
     if(!require(tidyverse))
         install.packages("tidyverse")
     library(tidyverse)
 
-    forder <- dirname(rstudioapi::getSourceEditorContext()$path)
-    return (sensors <- read.csv(paste0(folder,name)))
+    folder <- dirname(rstudioapi::getSourceEditorContext()$path)
+    parentFolder <- dirname(folder)
+    return (sensors <- read.csv(paste0(parentFolder,"/datasets",name_dataset),
+                                stringsAsFactors =  TRUE))
 }
